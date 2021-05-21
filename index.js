6b8fb5b997b11c08511e6303242c3e0ba9dfc45c
Justin <jdanielmcarthur@msn.com>
2021-05-21 14:42:11 -0700

    commit tree 3995aa51c8fd664432e1de81632d16a96ce19df9

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function batteryAdder(accumulator, currentValue) {
+    return accumulator + currentValue;
+}
+
+const totalBatteries = batteryBatches.reduce(batteryAdder, 0)
